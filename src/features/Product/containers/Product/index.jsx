73144c868c556c846e71bff483f7efa96de265c3
thanhhht <thanhhht1704@gmail.com>
2021-01-21 00:13:02 +0700

    create container Product

---
+++ src/features/Product/containers/Product/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Product() {
+  return (
+    <main className="main-content">
+      <div className="side-bar"></div>
+      <div className="content"></div>
+    </main>
+  );
+}
