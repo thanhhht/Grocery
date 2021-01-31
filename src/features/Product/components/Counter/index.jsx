fe635c441bb84b9a59739bc4f1928c95858f94f3
thanhhht <thanhhht1704@gmail.com>
2021-01-31 23:01:30 +0700

    add logic to component Counter

--- src/features/Product/components/Counter/index.jsx
+++ src/features/Product/components/Counter/index.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
-import {ReactComponent as PlusIcon} from "../../../../images/icons/plus.svg";
-import {ReactComponent as MinusIcon} from "../../../../images/icons/minus.svg";
+import React, { useState } from 'react';
+import { ReactComponent as PlusIcon } from '../../../../images/icons/plus.svg';
+import { ReactComponent as MinusIcon } from '../../../../images/icons/minus.svg';
 
 import './style.scss';
 
 export default function Counter() {
+  const [quantity, setQuantity] = useState(1);
+  function increaseQuantity() {
+    const numb = quantity + 1;
+    setQuantity(numb);
+  }
+  function decreaseQuantity() {
+    const numb = quantity - 1;
+    setQuantity(numb);
+  }
   return (
     <div className="counter">
-      <button><MinusIcon/></button>
-      <span className="counter-value">1</span>
-      <button><PlusIcon/></button>
+      <button onClick={() => decreaseQuantity()}>
+        <MinusIcon />
+      </button>
+      <span className="counter-value">{quantity}</span>
+      <button onClick={() => increaseQuantity()}>
+        <PlusIcon />
+      </button>
     </div>
   );
 }
