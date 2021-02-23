98af3d35af473ca6ab5a2a0d0cf93acc2b400f36
thanhhht <thanhhht1704@gmail.com>
2021-02-23 23:18:02 +0700

    commit

--- src/features/Product/containers/ProductSection/index.jsx
+++ src/features/Product/containers/ProductSection/index.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import Categories from '../../components/Categories';
 import ProductList from '../../components/ProductList';
 
